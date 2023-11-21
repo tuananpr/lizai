bb37ddc135b93fa0da12a273a317621916fea474
Tyler.Nguyen <tyler.nguyen@lizai.co>
2023-11-21 14:46:43 +0700

    Update 202311211426

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,11 +1,11 @@
-import org.testng.reporters.jq.Main;
-
-public class test {
-    int x = 10;
-
-    public static void main(String[] args) {
-        test myObj = new test();
-        myObj.x = 21;
-        System.out.println(myObj.x);
-    }
-}
+//import org.testng.reporters.jq.Main;
+//
+//public class test {
+//    int x = 10;
+//
+//    public static void main(String[] args) {
+//        test myObj = new test();
+//        myObj.x = 21;
+//        System.out.println(myObj.x);
+//    }
+//}
